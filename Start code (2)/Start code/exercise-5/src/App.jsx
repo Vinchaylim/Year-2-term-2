4fa0e634c5b487e9bb1de82284216d06f8749e5f
Vinchaylim <vinchay.lim@student.cadt.edu.kh>
2026-02-19 15:26:03 +0700

    skibidi

--- Start code (2)/Start code/exercise-5/src/App.jsx
+++ Start code (2)/Start code/exercise-5/src/App.jsx
@@ -1,5 +1,24 @@
+import { GAMES } from "./data";
+
 function App() {
-  return <>{/* Your code  here */}</>;
+  return (
+    <>
+      <header className="main-header">
+        <h1>Games</h1>
+      </header>
+
+      <div className="cards-container">
+        {GAMES.map((game) => (
+          <div key={game.id} className="card">
+            <img src={game.image} alt={game.name} className="card-image" />
+            <h2>{game.name}</h2>
+            <p className="Type-text">{game.Type}</p>
+           
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- Start code (2)/Start code/exercise-5/src/data.js
+++ Start code (2)/Start code/exercise-5/src/data.js
@@ -1,1 +1,23 @@
-/* Your data here */
+import wukong from "./assets/blackmyth.jpg";
+import eldenring from "./assets/images.jpg";
+import counterstrike from "./assets/CS2.jpg";
+export const GAMES = [
+  {
+    id: 1,
+    image: wukong,
+    name: "Wukong",
+    Type: "AAA, souls like",
+  },
+  {
+    id: 2,
+    image: eldenring,
+    name: "Elden Ring",
+    Type: "AAA, open world",
+  },
+  {
+    id: 3,
+    image: counterstrike,
+    name: "Counter Strike",
+    Type: "FPS, competitive",
+  }
+];
